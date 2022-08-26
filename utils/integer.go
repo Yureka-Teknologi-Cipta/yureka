3f6103ed26344ef9ab2d2193b817f565f57db6ea
Fahrur Rifai <mfahrurrifai@gmail.com>
2022-08-26 13:36:07 +0700

    make it simple

---
+++ utils/integer.go
@@ -0,0 +1,65 @@
+package helpers
+
+import (
+	"math/rand"
+	"time"
+)
+
+type Ints []int
+type Ints64 []int64
+
+func (slice Ints) Include(value int) bool {
+	for _, item := range slice {
+		if value == item {
+			return true
+		}
+	}
+	return false
+}
+
+func (slice Ints64) Include(value int64) bool {
+	for _, item := range slice {
+		if value == item {
+			return true
+		}
+	}
+	return false
+}
+
+func (slice Ints) Unique() Ints {
+	unique := Ints{}
+	for _, v := range slice {
+		if !unique.Include(v) {
+			unique = append(unique, v)
+		}
+	}
+
+	return unique
+}
+
+func (slice Ints64) Unique() Ints64 {
+	unique := Ints64{}
+	for _, v := range slice {
+		if !unique.Include(v) {
+			unique = append(unique, v)
+		}
+	}
+
+	return unique
+}
+
+func (slice Ints) Random() int {
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(slice), func(i, j int) { slice[i], slice[j] = slice[j], slice[i] })
+	n := rand.Int() % len(slice)
+
+	return slice[n]
+}
+
+func (slice Ints64) Random() int64 {
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(slice), func(i, j int) { slice[i], slice[j] = slice[j], slice[i] })
+	n := rand.Int() % len(slice)
+
+	return slice[n]
+}
---
+++ utils/strings.go
@@ -0,0 +1,36 @@
+package helpers
+
+import (
+	"math/rand"
+	"time"
+)
+
+type Strings []string
+
+func (slice Strings) Include(value string) bool {
+	for _, item := range slice {
+		if value == item {
+			return true
+		}
+	}
+	return false
+}
+
+func (slice Strings) Unique() Strings {
+	unique := Strings{}
+	for _, v := range slice {
+		if !unique.Include(v) {
+			unique = append(unique, v)
+		}
+	}
+
+	return unique
+}
+
+func (slice Strings) Random() string {
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(slice), func(i, j int) { slice[i], slice[j] = slice[j], slice[i] })
+	n := rand.Int() % len(slice)
+
+	return slice[n]
+}
